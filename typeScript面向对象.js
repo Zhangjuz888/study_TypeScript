53ad5c77c362df75a095fe8421f425996dc75033
青气南员方便财主 <12207795+qing-qinan@user.noreply.gitee.com>
2023-08-16 11:09:35 +0800

    feat: typeScript面向对象

---
+++ typeScript面向对象.js
@@ -0,0 +1,7 @@
+// typeScript定义的对象方式不同，它所对应的不单是 key和value 还有键值对的类型（type）
+var person = {
+    firstName: 'zhangsan',
+    lastName: '法外狂徒',
+    age: 81
+};
+console.log("person_____", person);
---
+++ typeScript面向对象.ts
@@ -0,0 +1,11 @@
+
+// typeScript定义的对象方式不同，它所对应的不单是 key和value 还有键值对的类型（type）
+const person = {
+    firstName: 'zhangsan',
+    lastName: '法外狂徒',
+    age: 81
+}
+
+console.log(`person_____`, person);
+
+
